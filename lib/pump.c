d2114c5e590576917bbc5401035bd211713303fb
albertMgarcia <albert.mengod@e-campus.uab.cat>
2018-05-28 14:16:17 +0200

    Update pump.c
    now we have pump_set_data and shell_pump_set_data to integer with the rest of the project

--- lib/pump.c
+++ lib/pump.c
@@ -20,10 +20,10 @@
 
 //Table to storage sensors data the size of the table depens on the number of sensors
 int table [5][2];
-bool pumpe_is_ON = false;
+bool pumpe_is_on = false;
 
 //Each time we open/close the pump we should reset the table values
-void reset_Table( int table[][2])
+void reset_table( int table[][2])
 {
 
 	for (int i=0;i<NUM_SENSORS;i++){
@@ -33,7 +33,7 @@ void reset_Table( int table[][2])
 }
 
 //Function that prints the table with the values just for visual information
-void print_Table( int table[][2])
+void print_table( int table[][2])
 {
 
 	for(int i=0;i<NUM_SENSORS;i++){
@@ -42,25 +42,25 @@ void print_Table( int table[][2])
 }
 
 
-bool main_Pump(struct DataStruct pump_data, bool pumpe_is_ON)
+bool pump_set_data(struct DataStruct pump_data, bool pumpe_is_on)
 {
 
-    int open_Pumpe = 0;
-    int close_Pumpe = 0;
-    int sum_Hum = 0;
-    int avg_Hum = 0;
+    int open_pumpe = 0;
+    int close_pumpe = 0;
+    int sum_hum = 0;
+    int avg_hum = 0;
     if (pump_data.data  < PUMP_THRESHOLD_VERYLOW || pump_data.data > PUMP_THRESHOLD_VERYHIGH){
 
-        if(pump_data.data < PUMP_THRESHOLD_VERYLOW && !pumpe_is_ON){
+        if(pump_data.data < PUMP_THRESHOLD_VERYLOW && !pumpe_is_on){
 		printf("OpenPumpe \n");
-		reset_Table(table);
-		pumpe_is_ON = true;
+		reset_table(table);
+		pumpe_is_on = true;
             //Here we should call the function that opens the pumpe
         }
 
-        if(pump_data.data  > PUMP_THRESHOLD_VERYHIGH && pumpe_is_ON){
+        if(pump_data.data  > PUMP_THRESHOLD_VERYHIGH && pumpe_is_on){
 	      printf("ClosePumpe");
-	      reset_Table(table);
+	      reset_table(table);
              //Here we should call the function that closes the pumpe
         }
 
@@ -68,26 +68,27 @@ bool main_Pump(struct DataStruct pump_data, bool pumpe_is_ON)
     }
 
     else{
-        bool repeated_Data = false;
+        bool repeated_data = false;
 	//If the sensor is already present in the table we update his value if not we add it to the table
         for(int i=0;i<NUM_SENSORS-1;i++){
 
             if(table[i][0]==pump_data.id){
-                repeated_Data = true;
+                repeated_data = true;
 		table[i][1] = pump_data.data;
 		printf("TableUpdated \n");
-		print_Table(table);
+		print_table(table);
             }
         }
-        if(!repeated_Data){
+        if(!repeated_data){
 	    int aux=0;
+	    //We look for the first free space in the table
 	    while(table[aux][0] != 0){
 		aux++;
 	    }
             table[aux][0] = pump_data.id;
             table[aux][1] = pump_data.data;
 	    printf("AddedToTable \n");
-	    print_Table(table);
+	    print_table(table);
         }
 
 	//When we got the values of all the sensors we operate with the values
@@ -95,42 +96,42 @@ bool main_Pump(struct DataStruct pump_data, bool pumpe_is_ON)
 	    printf("ALL SENSORS SENDED THE DATA \n");
             //Calculate the AvgHum
             for(int i=0;i<NUM_SENSORS-1;i++){
-                sum_Hum = sum_Hum + table[i][1];
+                sum_hum = sum_hum + table[i][1];
             }
-            avg_Hum = sum_Hum / NUM_SENSORS;
+            avg_hum = sum_hum / NUM_SENSORS;
 
-            if(avg_Hum < PUMP_THRESHOLD_LOW && avg_Hum > PUMP_THRESHOLD_VERYLOW){
-                open_Pumpe=1;
+            if(avg_hum < PUMP_THRESHOLD_LOW && avg_hum > PUMP_THRESHOLD_VERYLOW){
+                open_pumpe=1;
             }
 
-            if(avg_Hum < PUMP_THRESHOLD_VERYHIGH && avg_Hum > PUMP_THRESHOLD_HIGH && pumpe_is_ON){
-                close_Pumpe=1;
+            if(avg_hum < PUMP_THRESHOLD_VERYHIGH && avg_hum > PUMP_THRESHOLD_HIGH && pumpe_is_on){
+                close_pumpe=1;
             }
-            reset_Table(table);
+            reset_table(table);
 
         }
-        if(open_Pumpe==1 && !pumpe_is_ON){
+        if(open_pumpe==1 && !pumpe_is_on){
 		printf("OPENPUMPE \n");
-		pumpe_is_ON = true;
+		pumpe_is_on = true;
             //Here we should call the functions that opens the pumpe
             }
 
-        if(close_Pumpe==1 && pumpe_is_ON){
+        if(close_pumpe==1 && pumpe_is_on){
 	    	printf("CLOSEPUMPE \n");
-		pumpe_is_ON = false;
+		pumpe_is_on = false;
             //Here we should call the functions that closes the pumpe
             }
     }
-return pumpe_is_ON;
+return pumpe_is_on;
 }
 
-void main( int argc, char * argv[])
+void shell_pump_set_data( int argc, char * argv[])
 {
 	//We should change this insted of keyboard parameters, a functions should send the values
 	pump_data.id = strtol( argv[1],NULL,10);
 	pump_data.data = strtol( argv[2],NULL,10);
-	bool pumpe_State=false;
-	pumpe_State = main_Pump(pump_data,pumpe_State);
+	bool pumpe_state=false;
+	pumpe_state = pump_set_data(pump_data,pumpe_state);
 	int identifier=1;
 	int data_sensor=0;
 
@@ -140,7 +141,7 @@ void main( int argc, char * argv[])
 		pump_data.id=identifier;
 		pump_data.data=data_sensor;
 		printf("_____________________");
-		pumpe_State = main_Pump(pump_data,pumpe_State);
+		pumpe_state = pump_set_data(pump_data,pumpe_state);
 
 	}
 }
